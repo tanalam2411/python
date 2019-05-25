bb33c4d3b845496dd79a656e4c405f82bd692adc
tanveer <onlinejob.2411@gmail.com>
2019-05-26 03:44:39 +0530

    __main__ example

---
+++ python_beyond_the_basics/Lib/reader/__main__.py
@@ -0,0 +1,11 @@
+
+import sys
+
+from reader.reader import read_file
+
+
+read_file(sys.argv[1])
+
+
+
+
---
+++ python_beyond_the_basics/Lib/reader/reader/reader.py
@@ -0,0 +1,6 @@
+
+
+def read_file(filename):
+    with open(filename, 'rt') as fp:
+        print (fp.read())
+
