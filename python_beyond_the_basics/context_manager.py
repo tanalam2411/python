4e961edc450a30649aef98f5080e7aca15fd8bfc
tanveer <onlinejob.2411@gmail.com>
2019-07-17 23:41:09 +0530

    connection factory.

--- python_beyond_the_basics/context_manager.py
+++ python_beyond_the_basics/context_manager.py
@@ -1,3 +1,36 @@
+#! -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+
+class ConnectionFactory(metaclass=ABCMeta):
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        """
+        :return:
+        """
+
+    def get_connection(self):
+        pass
+
+    def __enter__(self):
+        """
+        :return:
+        """
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        :param exc_type:
+        :param exc_val:
+        :param exc_tb:
+        :return:
+        """
 
 
 class Test:
