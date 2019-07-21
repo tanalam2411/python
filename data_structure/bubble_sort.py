c7ba0e6ae471aa14603c088a2fd74d1d28f98267
tanveer <onlinejob.2411@gmail.com>
2019-07-21 23:52:34 +0530

    updated doc string

--- data_structure/bubble_sort.py
+++ data_structure/bubble_sort.py
@@ -3,8 +3,41 @@
 
 """
 Bubble sort implementation in Python.
+
+Input array = [5, 4, 3, 2, 1]
+
+- Ascending order
+
+In every iteration one of the largest number will be shifted to the end.
+
+So it will take N - 1 iteration if array is completely unsorted, meaning is
+case if we're doing asce order the input order would be in des order and vice
+versa.
+
+
 """
 
+
+
+input_array = [5, 4, 3, 2, 1]
+
+
+for i in range(len(input_array) - 1):
+    print 'i: ', i
+    print '-------'
+    for j in range(1, len(input_array) - i):
+
+        num1 = input_array[j-1]
+        num2 = input_array[j]
+
+        if num2 < num1:
+            input_array[j-1], input_array[j] = input_array[j], input_array[j-1]
+
+        print 'j: ', j
+
+    print '-------'
+
+print input_array
 
 
 
